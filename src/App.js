d3e7305ff3156daf75498ad4da2637d9b3c8d23c
Adrian Grimm <usmcamgrimm@gmail.com>
2020-03-07 14:16:48 -0500

    Start building this from scratch with React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './images/havamal.png'
+import './App.css'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='Havamal-logo' alt='logo' />
+        <p>Welcome, Hordes of Havamal</p>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
